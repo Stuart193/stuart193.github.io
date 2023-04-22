6283ce880ee65a88b0aff7a65ba391777a0fc98f
Stuart193 <117926180+Stuart193@users.noreply.github.com>
2023-04-22 18:27:11 -0400

    Delete bibleReader directory

--- bibleReader/scripts/interactions.js
+++
@@ -1,9 +0,0 @@
-
-Date.prototype.month = function () {
-    return this.getMonth() + 1;
-}
-Date.prototype.date = function () {
-    return this.getDate();
-}
-
-location.href = ((new Date().month()) + "/" + (new Date().date()) + ".html");
